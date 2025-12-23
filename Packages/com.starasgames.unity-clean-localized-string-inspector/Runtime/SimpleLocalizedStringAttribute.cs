14e6a7367d0d3dd077f7a93596ac9d5da3c46189
TarasPC <taras-pc@fake.mail>
2025-12-23 22:01:56 +0100

    main. Original code by Thomas "noio" van den Berg.  - Installed localization package.  - asmdef references.

---
+++ Packages/com.starasgames.unity-clean-localized-string-inspector/Runtime/SimpleLocalizedStringAttribute.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Utils.SimpleLocalizedStringDrawer
+{
+    public class SimpleLocalizedStringAttribute : PropertyAttribute
+    {
+        public string TableName { get; }
+        public bool Multiline { get; }
+
+        public SimpleLocalizedStringAttribute(string tableName, bool multiline = false)
+        {
+            TableName = tableName;
+            Multiline = multiline;
+        }
+    }
+}
